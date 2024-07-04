82126e7160fac0be4f81a10cdf067d9bda5fa912
Gilbert S Cachaya Caicedo <47873666+GilbertCC@users.noreply.github.com>
2024-07-04 18:57:44 -0500

    prefinal

--- js/main.js
+++ js/main.js
@@ -470,3 +470,25 @@
         
         
 })(jQuery);
+
+/*--- formulario ---*/
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+      method: this.method,
+      body: form,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    if (response.ok){
+      this.reset()
+      alert('Gracias su mensaje fue enviado , le contactaremos pronto')
+    }
+}
